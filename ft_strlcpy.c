2ea38da30f9397c22130ee99e4f586a6e162e57f
Maria Camila Pedraza Casasbuenas <mpedraza@f1r11s13.clusters.42paris.fr>
2025-11-11 14:01:48 +0100

    fix norminette

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: mpedraza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 18:57:10 by mpedraza          #+#    #+#             */
-/*   Updated: 2025/11/11 12:04:30 by mpedraza         ###   ########.fr       */
+/*   Updated: 2025/11/11 13:52:14 by mpedraza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t  ft_strlcpy(char *dst, const char *src, size_t siz)
+size_t	ft_strlcpy(char *dst, const char *src, size_t siz)
 {
 	size_t	i;
 
